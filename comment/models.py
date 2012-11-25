d2e2268e1579881f37ace34b77a1e94d50f0c55f
Ondrej Sika <sika.ondrej@gmail.com>
2012-11-25 14:06:22 +0100

    add comment model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from mapa.models import Poi
+
+
+class Comment(models.Model):
+    poi = models.ForeignKey(Poi)
+    name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return u"%s (%s) ~%i %s" % (self.name, self.email, self.pk, self.content[:20])
