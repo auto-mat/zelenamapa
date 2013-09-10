3d3bc7a4a4110bf04319f8584cac84aa90137e2b
Petr Dlouhý <petr.dlouhy@email.cz>
2013-09-10 23:34:01 +0200

    move wsgi to root directory

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for zelenamapa project.
+WSGI config for newprj project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -14,8 +14,28 @@ framework.
 
 """
 import os
+import site
+import sys
+from project.settings import PROJECT_DIR
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zm.settings")
+ALLDIRS = [ os.path.join(PROJECT_DIR, 'env/lib/python2.6/site-packages'), ]
+
+# Remember original sys.path.
+prev_sys_path = list(sys.path)
+
+# Add each new site-packages directory.
+for directory in ALLDIRS:
+  site.addsitedir(directory)
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
