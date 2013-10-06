319e729bb2c7b51a94f8f21a6935e0808dea7794
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-06 11:33:55 +0200

    backup db when update

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 sudo git pull
 env/bin/pip install -r requirements.txt
+sudo env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-mapa.json
 sudo env/bin/python manage.py migrate
 sudo env/bin/python manage.py collectstatic --noinput
 touch wsgi.py
