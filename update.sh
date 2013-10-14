927799cd5f8a068248ed745fa8a352f86121c941
Petr Dlouhý <petr.dlouhy@email.cz>
2013-10-14 17:04:18 +0200

    better update script

--- update.sh
+++ update.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-sudo git pull
-env/bin/pip install -r requirements.txt
-sudo env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-mapa.json
-sudo env/bin/python manage.py migrate
-sudo env/bin/python manage.py collectstatic --noinput
+git pull
+env/bin/pip install -r requirements
+if [ "$1" = "migrate" ]; then
+   echo "Backuping db..."
+   mkdir db_backup
+   env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-zmapa.json
+   echo "Migrating..."
+   env/bin/python manage.py migrate
+fi
+env/bin/python manage.py collectstatic --noinput
 touch wsgi.py
