3e1297cca6281a64b32722343299636d04668d35
Petr Dlouhý <petr.dlouhy@email.cz>
2013-11-05 21:13:58 +0100

    improve update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 git pull
-env/bin/pip install -r requirements.txt
+source env/bin/activate
+pip install -r requirements.txt
 if [ "$1" = "migrate" ]; then
    echo "Backuping db..."
    mkdir db_backup
-   env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-zmapa.json
+   ./manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-zmapa.json
    echo "Migrating..."
-   env/bin/python manage.py migrate
+   ./manage.py migrate
 fi
-env/bin/python manage.py collectstatic --noinput
+./manage.py collectstatic --noinput
 touch wsgi.py
