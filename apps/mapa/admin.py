7dedaf008e352cdb157d7a8a0bdbe88aedba85d6
Petr Dlouhý <petr.dlouhy@email.cz>
2014-12-12 21:21:43 +0100

    add make_pois action

--- apps/mapa/admin.py
+++ apps/mapa/admin.py
@@ -4,10 +4,12 @@
 # Import the admin site reference from django.contrib.admin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from webmap.models import Poi
+from webmap.models import Poi, Marker, Photo, License
 import webmap
 from django.contrib.auth.models import User
 from mapa.models import Upresneni, Staticpage, Sit, SitPoi
+from django.contrib.gis.admin import OSMGeoAdmin
+from django.utils.html import mark_safe
 
 
 class UserAdmin(UserAdmin):
@@ -45,12 +47,45 @@ class SitInline(admin.TabularInline):
 class Webmap_PoiAdmin(webmap.admin.PoiAdmin):
     inlines = webmap.admin.PoiAdmin.inlines + [SitPoiInline, SitInline, ]
 
+def make_photo(photo_field, email, poi):
+    if photo_field:
+        photo = Photo(
+                photo=photo_field,
+                photographer=email,
+                license=License.objects.get(id=1),
+                poi=poi,
+                )
+        photo.save()
 
-class UpresneniAdmin(admin.ModelAdmin):
+def make_pois(modeladmin, request, queryset):
+    added_pois = []
+    for upresneni in queryset:
+        poi = Poi(
+                name=upresneni.name,
+                desc=upresneni.desc,
+                geom=upresneni.location,
+                url=upresneni.url,
+                address=upresneni.address,
+                remark=u"Vytvořeno z upřesnění zadaného s emailem %s" % upresneni.email,
+                marker=Marker.objects.get(slug='new'),
+                )
+        poi.save()
+        make_photo(upresneni.photo1, upresneni.email, poi)
+        make_photo(upresneni.photo2, upresneni.email, poi)
+        make_photo(upresneni.photo3, upresneni.email, poi)
+        added_pois.append(poi)
+
+    added_pois_string = ", ".join([("<a href='/admin/webmap/poi/%s'>%s</a>" % (p.id, p.name)) for p in added_pois])
+    modeladmin.message_user(request, mark_safe(u"Vytvořeny následující místa: %s" % added_pois_string))
+make_pois.short_description = u"Vytvořit nová POI"
+
+
+class UpresneniAdmin(OSMGeoAdmin):
     model = Upresneni
     raw_id_fields = ('webmap_poi',)
     list_filter = ('status',)
-    list_display = ('name', 'webmap_poi', 'created', 'email', 'status', 'desc',)
+    list_display = ('webmap_poi', 'name', 'created', 'email', 'status', 'desc',)
+    actions = [make_pois]
 
 
 class StaticAdmin(admin.ModelAdmin):
