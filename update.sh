70bc90277743825e95c9cf4532828e69834c266e
Petr Dlouhý <petr.dlouhy@email.cz>
2020-05-13 09:41:35 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -6,9 +6,9 @@ pip install -r requirements.txt
 if [ "$1" = "migrate" ]; then
    echo "Backuping db..."
    mkdir db_backup
-   ./manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-zmapa.json
+   python ./manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-zmapa.json
    echo "Migrating..."
-   ./manage.py migrate
+   python ./manage.py migrate
 fi
-./manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 touch wsgi.py
